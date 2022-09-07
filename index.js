0385fbafbe59add148b15ce464399c7ac127292c
David <david.v.bernardo@gmail.com>
2022-09-07 14:42:40 -0500

    commit tree 77f351afbee478423befb3dc3ebf1b8f2720da5e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; ++i)
+    {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return false;
+}
+
+function writeCards(names, occasion) {
+    let msgs = []
+    for (let i = 0; i < gifts.length; ++i)
+    {
+        msgs.push("Thank you, " + names[i] + ", for the wonderful " + occasion + " gift!")
+    }
+    return msgs;
+}
+
+function countDown(n) {
+    while (n >= 0)
+    {
+        console.log(n--);
+    }
+}
+
+wrapGifts(gifts);
